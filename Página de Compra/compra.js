fcc5256b85ab4690b91a13d1ec5411b61c4d3357
Prof-Davi-Git <davigodoi@prof.educacao.sp.gov.br>
2026-08-18 01:34:04 -0300

    novamudança
    alteração

--- Página de Compra/compra.js
+++ Página de Compra/compra.js
@@ -1,9 +1,29 @@
-function trocarImagem(imagem) {
+function trocarModelo(imagem) {
+
 
     let imagemPrincipal =
         document.getElementById("imagemPrincipal");
 
+
+    let nomeModelo =
+        document.getElementById("nomeModelo");
+
+
+    let especificacaoModelo =
+        document.getElementById("especificacaoModelo");
+
+
+
     imagemPrincipal.src =
         imagem.src;
+
+
+    nomeModelo.textContent =
+        imagem.dataset.nome;
+
+
+    especificacaoModelo.textContent =
+        imagem.dataset.especificacao;
+
 
 }
